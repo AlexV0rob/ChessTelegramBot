f4d0e0948e08aeeca47bda10f909956713dbf292
aleks <aleks@MSI>
2025-10-07 19:24:10 +0500

    Исправления замечаний и перевод Javadoc и комментариев
    Код почищен от лишних комментариев.
    Оставленные комментарии и Javadoc переведены на русский.
    В классе MainLogicTest удалены методы, проверяющие работу
    пользовательского ввода /start и /help за ненадобностью.

--- src/test/java/org/example/MainLogicTest.java
+++ src/test/java/org/example/MainLogicTest.java
@@ -1,10 +1,10 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 
 /**
- * Тестирование класса MainLogic.
+ * Тестирование класса MainLogic
  */
 class MainLogicTest {
 	/**
@@ -12,44 +12,8 @@ class MainLogicTest {
 	 */
     @Test
     void echoMessageTest() {
-        assertEquals(
+        Assertions.assertEquals(
         		"Вы отправили: TEST",
-        		
-        		new MainLogic().processInput("TEST")
-        		);
-    }
-    
-    /**
-	 * Проверка метода startCommand
-	 */
-    @Test
-    void startCommandTest() {
-        assertEquals("Здравствуй, путник! Я бот о шахматах. Сейчас я умею:\n"
-        		+ " - возвращать твои сообщения;\n"
-        		+ " - показывать окно помощи;\n\n"
-        		+ "Пока что я могу только это,"
-        		+ " но список возможностей будет пополняться с течением разработки."
-        		+ " Отправь /help для большей информации.",
-        		
-        		new MainLogic().processInput("/start")
-        		);
-    }
-    
-    /**
-	 * Проверка метода helpCommand
-	 */
-    @Test
-    void helpCommandTest() {
-        assertEquals(
-        		"Сейчас я могу:\n"
-        		+ " - возврашать твоё сообщение;\n"
-        		+ " - показывать это окно;\n\n"
-        		+ "Доступные команды:\n"
-        		+ "/help - позволяет это сообщение\n"
-        		+ "/start - перезапускает бота\n\n"
-        		+ "Скоро будет больше возможностей.",
-        		
-        		new MainLogic().processInput("/help")
-        		);
+        		new MainLogic().processInput("TEST"));
     }
 }
