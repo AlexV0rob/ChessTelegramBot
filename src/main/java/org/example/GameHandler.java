3860b64f4055d9a192781348a5928f33a2a21a71
Никита <@192.168.0.123>
2025-10-13 21:49:44 +0500

    Небольшие изменения Исправлено название класса пользователя Изменено условие для switch-case

--- src/main/java/org/example/GameHandler.java
+++ src/main/java/org/example/GameHandler.java
@@ -1,51 +1,51 @@
 package org.example;
-
-import org.telegram.telegrambots.meta.api.objects.Update;
 /**
  * Класс для произведения хода у конкретного пользователя
  */
 public class GameHandler{
-	public boolean ProgressHandler(Player player, int rawStartPos, int rawEndPos, byte figureCode,boolean isWhite)
+	public boolean ProgressHandler(User user, int rawStartPos, int rawEndPos, byte figureCode)
 	{
 		boolean isNormalMove;
-		switch(figureCode)
+		byte[] curDesk = user.getDesk();
+		switch((figureCode + 1) % 2)
 		{
 		case 1:
 			Chessmen pawn = new Pawn();
-			isNormalMove = pawn.CheckMove(rawStartPos, rawEndPos, player.chessDesk, isWhite);
+			isNormalMove = pawn.CheckMove(rawStartPos, rawEndPos,curDesk, user.doesWhitesMove());
 			break;
 		case 2:
 			Chessmen castle = new Castle();
-			isNormalMove = castle.CheckMove(rawStartPos, rawEndPos, player.chessDesk, isWhite);
+			isNormalMove = castle.CheckMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
 			break;
 		case 3:
 			Chessmen bishop = new Bishop();
-			isNormalMove = bishop.CheckMove(rawStartPos, rawEndPos, player.chessDesk, isWhite);
+			isNormalMove = bishop.CheckMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
 			break;
 		case 4:
 			Chessmen knight = new Knight();
-			isNormalMove = knight.CheckMove(rawStartPos, rawEndPos, player.chessDesk, isWhite);
+			isNormalMove = knight.CheckMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
 			break;
 		case 5:
 			Chessmen queen = new Queen();
-			isNormalMove = queen.CheckMove(rawStartPos, rawEndPos, player.chessDesk, isWhite);
+			isNormalMove = queen.CheckMove(rawStartPos, rawEndPos,curDesk, user.doesWhitesMove());
 			break;
 
 		case 6:
 			Chessmen king = new King();
-			isNormalMove = king.CheckMove(rawStartPos, rawEndPos, player.chessDesk, isWhite);
+			isNormalMove = king.CheckMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
 			break;
 		}
 		if(isNormalMove)
 		{
-			byte tmp = player.chessDesk[rawStartPos] ;
-			player.chessDesk[rawStartPos] = 0;
-			player.chessDesk[rawEndPos] = tmp;
+			byte tmp = curDesk[rawStartPos] ;
+			curDesk[rawStartPos] = 0;
+			curDesk[rawEndPos] = tmp;
 			return true;
 		}
 		else
 			return false;
 	}
+	
 	
 }
 
