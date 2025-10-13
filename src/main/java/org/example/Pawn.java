c54c7e0195956701d553b410694c52924d9130a4
Никита <@192.168.0.123>
2025-10-13 10:19:09 +0500

    Изменение класса пешки Добавление возможности делать двойной ход из стартовой точки

--- src/main/java/org/example/Pawn.java
+++ src/main/java/org/example/Pawn.java
@@ -7,12 +7,15 @@ public class Pawn implements  Chessmen{
 		Position startPos= new Position(rawStartPos);
 		if(chessDesk[rawEndPos] == 0){
 
-			if((endPos.line - startPos.line == 1) && (startPos.column == endPos.column))
+			if((Math.abs(endPos.line - startPos.line) == 1) && (startPos.column == endPos.column))
+				return true;
+			else if((startPos.line == 1 ||  startPos.line == 6)&&
+					((Math.abs(endPos.line - startPos.line) == 2) && (startPos.column == endPos.column)))
 				return true;
 		}
 		else if(chessDesk[rawEndPos] % 2 != isBlack)
 		{
-			if((endPos.line - startPos.line == 1) && (Math.abs(startPos.column -  endPos.column)== 0))
+			if((Math.abs(endPos.line - startPos.line) == 1) && (Math.abs(startPos.column -  endPos.column) == 1))
 				return true;
 		}
 		return false;
