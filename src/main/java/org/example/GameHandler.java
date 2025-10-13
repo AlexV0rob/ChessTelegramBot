af05864c3bbbe323817895143821765a70cde016
Никита <@192.168.0.123>
2025-10-14 00:58:03 +0500

    Небольшая правка в последовательности команд

--- src/main/java/org/example/GameHandler.java
+++ src/main/java/org/example/GameHandler.java
@@ -6,35 +6,38 @@ public class GameHandler{
 	public boolean ProgressHandler(User user, int rawStartPos, int rawEndPos, byte figureCode)
 	{
 		boolean isNormalMove = false;
-		if((rawStartPos > 63 || rawStartPos < 0 )&&(rawEndPos > 63 || rawEndPos < 0))
+		if((rawStartPos > 63 || rawStartPos < 0 ) || (rawEndPos > 63 || rawEndPos < 0))
 			return false;
 		byte[] curDesk = user.getDesk();
 		switch((figureCode + 1) % 2)
 		{
 		case 1:
 			Chessmen pawn = new Pawn();
-			isNormalMove = pawn.CheckMove(rawStartPos, rawEndPos,curDesk, user.doesWhitesMove());
+			isNormalMove = pawn.checkMove(rawStartPos, rawEndPos,curDesk, user.doesWhitesMove());
 			break;
 		case 2:
 			Chessmen castle = new Castle();
-			isNormalMove = castle.CheckMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
+			isNormalMove = castle.checkMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
 			break;
+
+
 		case 3:
-			Chessmen bishop = new Bishop();
-			isNormalMove = bishop.CheckMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
-			break;
-		case 4:
 			Chessmen knight = new Knight();
-			isNormalMove = knight.CheckMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
+			isNormalMove = knight.checkMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
+			break;
+
+		case 4:
+			Chessmen bishop = new Bishop();
+			isNormalMove = bishop.checkMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
 			break;
 		case 5:
 			Chessmen queen = new Queen();
-			isNormalMove = queen.CheckMove(rawStartPos, rawEndPos,curDesk, user.doesWhitesMove());
+			isNormalMove = queen.checkMove(rawStartPos, rawEndPos,curDesk, user.doesWhitesMove());
 			break;
 
 		case 6:
 			Chessmen king = new King();
-			isNormalMove = king.CheckMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
+			isNormalMove = king.checkMove(rawStartPos, rawEndPos, curDesk, user.doesWhitesMove());
 			break;
 		}
 		if(isNormalMove)
