e87d124155768f1105381ffa557042872fec30a0
Никита <@192.168.0.123>
2025-10-15 08:26:32 +0500

    Изменение описания методов

--- src/main/java/org/example/Bishop.java
+++ src/main/java/org/example/Bishop.java
@@ -10,7 +10,7 @@ public class Bishop implements  Chessmen{
 		  2) Проверяем что слон может так сходить
 		 * */
 		
-		 if (((chessDesk[rawEndPos] == 0 ) || ((chessDesk[rawEndPos] % 2 == 0 ) != isWhite))){  
+		 if (((chessDesk[rawEndPos] == 0 ) || ((chessDesk[rawEndPos] % 2 == 0 ) != isWhite)) && IsThereObstacle(rawStartPos, rawEndPos,chessDesk)){  
 			Position endPos= new Position(rawEndPos);
 			Position startPos= new Position(rawStartPos);
 			 if(Math.abs(endPos.line - startPos.line) == Math.abs(startPos.column - endPos.column))
