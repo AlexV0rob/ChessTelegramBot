430f8185ff14531bc90b136c4590cc0e788aeecc
Никита <nanedoshopa@gmail.com>
2025-11-15 18:32:35 +0500

    Удаление ненужного static

--- src/main/java/org/example/chess/Queen.java
+++ src/main/java/org/example/chess/Queen.java
@@ -10,11 +10,11 @@ public class Queen implements Chessmen {
     /**
      * ладья для проверки вертикальных и горизонтальных ходов
      */
-    private static Chessmen ROOK;
+    private Chessmen ROOK;
     /**
      * слон для проверки диагональных ходов
      */
-    private static Chessmen BISHOP;
+    private Chessmen BISHOP;
 
     /**
      * Конструктор класса
