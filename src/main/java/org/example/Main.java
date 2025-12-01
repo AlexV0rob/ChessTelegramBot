9a2c1305d22b00a4f814f0f23b5dec00a134b304
Никита <nanedoshopa@gmail.com>
2025-12-01 11:55:25 +0500

    Исправление ошибок в VKbot

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 import java.sql.SQLException;
 
+import api.longpoll.bots.exceptions.VkApiException;
 import org.example.bots.TelegramBot;
+import org.example.bots.VKBot;
 import org.example.statesHandlers.DatabaseStatesHandler;
 import org.example.statesHandlers.MemoryStatesHandler;
 import org.example.statesHandlers.StatesHandler;
@@ -12,29 +14,33 @@ import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
  * Основной класс, где происходит запуск бота
  */
 public class Main {
-	/**
-	 * Точка входа программы. 
-	 */
-	public static void main(String[] args) {
-		String botToken = System.getenv("telegram_bot_token");
-		String databaseURL = "jdbc:sqlite:./src/main/resources/states.db";
-		StatesHandler statesHandler;
-		try {
-			statesHandler = new DatabaseStatesHandler(databaseURL);
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-			statesHandler = new MemoryStatesHandler();			
-		}
-		MainLogic mainLogic = new MainLogic(statesHandler);
+    /**
+     * Точка входа программы.
+     */
+    public static void main(String[] args) throws VkApiException {
+        String botToken = System.getenv("telegram_bot_token");
+        String databaseURL = "jdbc:sqlite:./src/main/resources/states.db";
+        StatesHandler statesHandler;
         try {
-        	TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+            statesHandler = new DatabaseStatesHandler(databaseURL);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            statesHandler = new MemoryStatesHandler();
+        }
+        MainLogic mainLogic = new MainLogic(statesHandler);
+        try {
+            TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
             botsApplication.registerBot(botToken, new TelegramBot(botToken, mainLogic));
             System.out.println("Телеграм бот запущен");
+            System.out.println("ВК бот запущен");
+            new VKBot(mainLogic).startPolling();
             Thread.currentThread().join();
         } catch (Exception e) {
-        	System.out.println("Couldn't connect to telegram");
+            System.out.println("Couldn't connect to telegram");
             e.printStackTrace();
         }
+
+
     }
 }
--- src/main/java/org/example/bots/VKBot.java
+++ src/main/java/org/example/bots/VKBot.java
@@ -9,55 +9,67 @@ import api.longpoll.bots.model.objects.basic.Message;
 import api.longpoll.bots.model.events.messages.MessageNew;
 
 public class VKBot extends LongPollBot implements Bot {
-	/**
-	 * Экземпляр класса MainLogic, обрабатывает входящие сообщения
-	 */
-	private final MainLogic logic;
- public VKBot(MainLogic mainLogic) {
-	 logic = mainLogic;
- }
- /**
-  * Обработать текстовое сообщение, полученное от пользователя
-  */
- private void processTextMessage(String incomingMessage, long chatId) {
- 	logic.processInput(this, incomingMessage, chatId);
- }
-@Override 
-public String getAccessToken(){
-	return "Тут нужен токен бота";
-}
-@Override
-public void onMessageNew(MessageNew  messageNew ) {
-	         Message message = messageNew.getMessage();
-	         if (message.hasText()) {
-	        	 processTextMessage(message.getText(),message.getPeerId());
-	        	 }
+    /**
+     * Экземпляр класса MainLogic, обрабатывает входящие сообщения
+     */
+    private final MainLogic logic;
 
-}
+    public VKBot(MainLogic mainLogic) {
+        logic = mainLogic;
+    }
 
-private int sendMessage(long chatId, String messageText) {
-    try {
-        vk.messages.send()
-        .setPeerId(Math.toIntExact(chatId))
-        .setMessage(messageText)
-        .execute();
-        return Math.toIntExact(chatId);
-        
-     } catch (VkApiException e) {
-         e.printStackTrace();
-         return -1;
-     }
-    
-}
-@Override
-public long sendMessages(long chatId, List<String> messagesTexts) {
-	// TODO Auto-generated method stub
-	return 0;
-}
-@Override
-public void editMessage(long chatId, long messageId, String editedMessageText, boolean moreMessages) {
-	// TODO Auto-generated method stub
-	
-}
+    /**
+     * Обработать текстовое сообщение, полученное от пользователя
+     */
+    private void processTextMessage(String incomingMessage, long chatId) {
+        logic.processInput(this, incomingMessage, chatId);
+    }
+
+    @Override
+    public String getAccessToken() {
+        return "vk1.a.Ml3xWPSnkVxoYUgR7UgfzJXLNE5Cqj6AqC4KmuGOBE-" +
+                "rgrKsk1GswCnRJbf0v4joxmITV-nXJo8Towlh_toWSAc9Ig0nXuayE5sK8avNr4_" +
+                "Fr42zxLbEG8Ml5nMg5_RZDsSRXYaXn_3ss_Pj60b4JR2GTdP9kWa2Xj6EBTmj4ECQqx2HBeVDIHYYezXzL7jW57v8iBP5Qu" +
+                "-K-gpO4v82wQ";
+    }
+
+    @Override
+    public void onMessageNew(MessageNew messageNew) {
+        Message message = messageNew.getMessage();
+        if (message.hasText()) {
+            processTextMessage(message.getText(), message.getPeerId());
+        }
+
+    }
+
+    private int sendMessage(long chatId, String messageText) {
+        try {
+            vk.messages.send()
+                    .setPeerId(Math.toIntExact(chatId))
+                    .setMessage(messageText)
+                    .execute();
+            return Math.toIntExact(chatId);
+
+        } catch (VkApiException e) {
+            e.printStackTrace();
+            return -1;
+        }
+
+    }
+
+    @Override
+    public long sendMessages(long chatId, List<String> messagesTexts) {
+        String lastMessage = messagesTexts.removeLast();
+        for (int i = 0; i < messagesTexts.size(); ++i) {
+            sendMessage(chatId, messagesTexts.get(i));
+        }
+        return chatId;
+    }
+
+    @Override
+    public void editMessage(long chatId, long messageId, String editedMessageText, boolean moreMessages) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
